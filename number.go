8670729d4b0f2ddb32d0204915fbfbee716689b9
hanfeng <hanfeng@zdns.cn>
2017-08-27 02:11:34 +0800

    fix eth exceed limit

--- number.go
+++ number.go
@@ -7,9 +7,22 @@ import (
 const ethToWei = float64(1000000000000000000)
 
 func EthToWei(ether float64) *big.Int {
-	return big.NewInt(int64(ether * ethToWei))
+	if ether < 1 {
+		return big.NewInt(int64(ether * ethToWei))
+	} else {
+		var result big.Int
+		result.Mul(big.NewInt(int64(ether)), big.NewInt(int64(ethToWei)))
+		return &result
+	}
 }
 
 func WeiToEth(wei *big.Int) float64 {
-	return float64(wei.Int64()) / ethToWei
+	eth := big.NewInt(1000000000000000000)
+	if wei.Cmp(eth) == 1 {
+		var result big.Int
+		result.Div(wei, eth)
+		return float64(result.Int64())
+	} else {
+		return float64(wei.Uint64()) / ethToWei
+	}
 }
--- number_test.go
+++ number_test.go
@@ -10,12 +10,18 @@ import (
 func TestEtherToWei(t *testing.T) {
 	wei := big.NewInt(1000000000000000000)
 	ut.Equal(t, wei, EthToWei(1))
+
+	wei.SetString("100000000000000000000", 10)
+	ut.Equal(t, wei, EthToWei(100))
+
+	wei.SetString("100000000000000000000000", 10)
+	ut.Equal(t, wei, EthToWei(100000))
 }
 
 func TestWeiToEither(t *testing.T) {
 	wei := big.NewInt(100000000000000000)
 	ut.Equal(t, float64(0.1), WeiToEth(wei))
 
-	wei = big.NewInt(1)
-	ut.Equal(t, float64(0.000000000000000001), WeiToEth(wei))
+	wei.SetString("100000000000000000000000", 10)
+	ut.Equal(t, float64(100000), WeiToEth(wei))
 }
